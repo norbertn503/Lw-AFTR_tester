be6c41aea37ac4d6a4d0ba23ce9d2436cbfad8a3
norbertn503 <norbertn503@gmail.com>
2024-09-15 19:10:46 +0000

    add output for user

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <errno.h>
 #include <sys/queue.h>
+#include <iostream>
 
 #include <rte_memory.h>
 #include <rte_launch.h>
@@ -18,21 +19,21 @@
 #include <rte_ip.h>
 #include <rte_udp.h>
 
-#define LEFTPORT 0
-#define RIGHTPORT 1
 
-char coresList[101]; // buffer for preparing the list of lcores for DPDK init (like a command line argument)
-char numChannels[11]; // buffer for printing the number of memory channels into a string for DPDK init (like a command line argument)
 
-int main(int argc, char **argv){
+int init(char **list){
+	#define LEFTPORT 0
+	#define RIGHTPORT 1
 
-    int ret;
+	char coresList[101]; // buffer for preparing the list of lcores for DPDK init (like a command line argument)
+	char numChannels[11]; // buffer for printing the number of memory channels into a string for DPDK init (like a command line argument)
+	int ret;
 	unsigned lcore_id;
 
 	const char *rte_argv[6]; // parameters for DPDK EAL init, e.g.: {NULL, "-l", "4,5,6,7", "-n", "2", NULL};
   	int rte_argc = static_cast<int>(sizeof(rte_argv) / sizeof(rte_argv[0])) - 1; // argc value for DPDK EAL init
 
-	rte_argv[0]=argv[0]; 	// program name
+	rte_argv[0]=list[0]; 	// program name
     rte_argv[1]="-l";	// list of lcores will follow
 
 	int cpu_left_sender = 4;
@@ -40,13 +41,13 @@ int main(int argc, char **argv){
 	int memory_channels = 4;
 
     snprintf(coresList, 101, "0,%d,%d", cpu_left_sender, cpu_right_receiver); // only forward (left to right) is active 
-    
+    std::cout << coresList << std::endl;
 
-  rte_argv[2]=coresList;
-  rte_argv[3]="-n";
-  snprintf(numChannels, 11, "%hhu", memory_channels);
-  rte_argv[4]=numChannels;
-  rte_argv[5]=0;
+  	rte_argv[2]=coresList;
+  	rte_argv[3]="-n";
+  	snprintf(numChannels, 11, "%hhu", memory_channels);
+  	rte_argv[4]=numChannels;
+  	rte_argv[5]=0;
 
 	
 
@@ -55,12 +56,24 @@ int main(int argc, char **argv){
 		rte_panic("Cannot init EAL\n");
 	/* >8 End of initialization of Environment Abstraction Layer */
 
-	rte_eth_dev_is_valid_port(LEFTPORT);
-	rte_eth_dev_is_valid_port(RIGHTPORT);
+	if(rte_eth_dev_is_valid_port(LEFTPORT)){
+		std::cout << "PORT OK" << std::endl;
+	} else {std::cout << "PORT NOT OK" << std::endl; }
+
+	if(rte_eth_dev_is_valid_port(RIGHTPORT)){
+		std::cout << "PORT OK" << std::endl;		
+	} else { std::cout << "PORT NOT OK" << std::endl; }
 	//rte_eal_mp_wait_lcore();
+
 
 	/* clean up the EAL */
 	rte_eal_cleanup();
+	return 0;
+}
+
+int main(int argc, char **argv){
+	std::cout << "STARTED" << std::endl;
+    init(argv);
 
 	return 0;
 }
