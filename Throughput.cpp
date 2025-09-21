ef710f78bab679a78dfc06ffea88c39420d2c9f5
norbertn503 <norbertn503@gmail.com>
2025-09-21 11:44:29 +0200

    psid input check

--- Throughput.cpp
+++ Throughput.cpp
@@ -413,8 +413,8 @@ int Throughput::readlwB4Data(const char *filename) {
         }
       } else if ( (pos = findKey(line, "psid")) >= 0 ) {
         sscanf(line+pos, "%u", &tmp_obj.psid);
-        if ( tmp_obj.psid < 0 && (tmp_obj.psid < pow(2.0, tmp_obj.psid_length)) ) {
-          std::cerr << "ERROR: PSID cannot be negative or greater then 2**psid_length" << std::endl;
+        if ( tmp_obj.psid >= pow(2.0, tmp_obj.psid_length) ) {
+          std::cerr << "ERROR: PSID cannot be greater then 2**psid_length" << std::endl;
           return -1;
         }
       }
